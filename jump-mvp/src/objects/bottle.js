35494526f6c27904f26abad5b90edf95bab97d73
wangdongxu <wangdongxu@vipkid.com.cn>
2019-04-28 23:02:17 +0800

    设置高光纹理

--- jump-mvp/src/objects/bottle.js
+++ jump-mvp/src/objects/bottle.js
@@ -1,23 +1,22 @@
-import bottleConfig from '../../config/bottle.config'
-import blockConfig from '../../config/block.config'
+import bottleConfig from '../../config/bottle.config';
+import blockConfig from '../../config/block.config';
 
 class Bottle {
     constructor(x, y, z) {
     }
 
     init() {
+        // 加载纹理loader;
+        this.loader = new THREE.TextureLoader();
+
         // 存放bottle的整体
         this.obj = new THREE.Object3D();
         this.obj.name = 'bottle';
         this.obj.position.set(bottleConfig.initPosition.x, bottleConfig.initPosition.y + blockConfig.height / 2, bottleConfig.initPosition.z)
-    
+        
+        const { specularMaterial,  middleMaterial, bottomMaterial } = this.loadTexture();
         // 存储瓶子的组合
         this.bottle = new THREE.Object3D();
-
-        // 定义一个基础的纹理, phong模型可以更好的体现高光
-        let basicMaterial = new THREE.MeshPhongMaterial({
-            color: 0x800080,
-        })
 
         // 计算菱形头部的大小
         let headRadius = bottleConfig.headRadius;
@@ -25,7 +24,7 @@ class Bottle {
         // 接受两个参数， 一个geometry， material 纹理
         this.head = new THREE.Mesh(
             new THREE.OctahedronGeometry(headRadius),
-            basicMaterial,
+            bottomMaterial,
         );
 
         this.head.castShadow = true;
@@ -35,17 +34,18 @@ class Bottle {
             new THREE.CylinderGeometry(
                 headRadius / 1.4, headRadius / 1.44 * 0.88, headRadius * 1.2, 20
             ),
-            basicMaterial
+            middleMaterial
         );
         this.middle.castShadow = true;
         this.middle.position.y = 1.3857 * headRadius;
         this.middle.position.x = 0;
         this.middle.position.z = 0;
-
+                
+      
         // 人物底部， 圆台
         this.bottom = new THREE.Mesh(
             new THREE.CylinderGeometry(0.62857 * headRadius, 0.907143 * headRadius, 1.91423 * headRadius, 20),
-            basicMaterial
+            bottomMaterial
         );
         
         // 设置半球
@@ -53,8 +53,9 @@ class Bottle {
         this.topGeometry.scale(1, 0.54, 1);
         this.top = new THREE.Mesh(
             this.topGeometry,
-            basicMaterial
+            bottomMaterial
         );
+
         
         this.top.castShadow = true;
         this.top.position.y = 1.8143 * headRadius;
@@ -79,6 +80,32 @@ class Bottle {
         this.bottle.position.x = 0;
 
         this.obj.add(this.bottle);
+    }
+
+    loadTexture() {
+        // 给人物的head添加纹理
+        const specularTexture = this.loader.load('../../resource/images/head.png');
+
+        // 定义一个高光材质的纹理,
+        const specularMaterial = new THREE.MeshBasicMaterial({
+            map: specularTexture
+        });
+
+        const middleTexture = this.loader.load('/game/resource/images/top.png');
+        const middleMaterial = new THREE.MeshBasicMaterial({
+            map: middleTexture
+        })
+
+        const bottomTexture = this.loader.load('/game/resource/images/bottom.png');
+        const bottomMaterial = new THREE.MeshBasicMaterial({
+            map: bottomTexture
+        })
+
+        return {
+            specularMaterial, 
+            middleMaterial,
+            bottomMaterial
+        }
     }
 
 
