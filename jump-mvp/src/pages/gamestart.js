b153c3f244ae1968c97bcc78824757f9ecc4fa3a
wangdongxu <wangdongxu@vipkid.com.cn>
2019-04-28 08:48:50 +0800

    增加bottle人物

--- jump-mvp/src/pages/gamestart.js
+++ jump-mvp/src/pages/gamestart.js
@@ -2,6 +2,7 @@ import { scene } from '../scene/index';
 import Cuboid from '../block/cuboid';
 import Cylinder from '../block/cylinder';
 import ground from '../objects/ground';
+import bottle from '../objects/bottle'
 
 class GameStart {
 
@@ -13,10 +14,13 @@ class GameStart {
         console.log('game init page show');
         this.scene = scene;
         this.ground = ground;
+        this.bottle = bottle;
         this.scene.init();
         this.ground.init();
+        this.bottle.init();
         this.addBlock();
         this.addGround();
+        this.addBottle();
         this.render();
         
     }
@@ -48,6 +52,11 @@ class GameStart {
 
     restart() {
         console.log('game page restart')
+    }
+
+    // 添加bottle（瓶子）
+    addBottle() {
+        this.scene.instance.add(this.bottle.obj);
     }
 }
 
--- jump-mvp/src/scene/camera.js
+++ jump-mvp/src/scene/camera.js
@@ -8,7 +8,7 @@ class Camera {
         // 透视相机和正焦相机（无近大远小的效果）
         const aspect = window.innerHeight / window.innerWidth;
         this.instance = new THREE.OrthographicCamera(-sceneConf.frustumSize, sceneConf.frustumSize, sceneConf.frustumSize * aspect, -sceneConf.frustumSize * aspect, -100, 85);
-        this.instance.position.set(-10, 10, 10);
+        this.instance.position.set(0, 0, 10);
         this.target = new THREE.Vector3(0, 0, 0);
         this.instance.lookAt(this.target);
     }
